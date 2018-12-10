2573cd3e0e1704505e9272bf5c2b1b78e7238253
Ramdhani <dhaniverdant23@gmail.com>
2018-12-10 19:15:17 +0700

    Component7

---
+++ src/components/Component7/Component7.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+export default class Component7 extends Component{
+
+  render(){
+    return(
+      <Text>C7</Text>
+    );
+  }
+}
+
+AppRegistry.registerComponent('Component7, () => Component7');
