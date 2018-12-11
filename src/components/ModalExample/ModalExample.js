5f9d55558325353352a2b135acd42b102c3c8eb6
Ramdhani <dhaniverdant23@gmail.com>
2018-12-11 23:33:26 +0700

    tidying Modal

--- src/components/ModalExample/ModalExample.js
+++ src/components/ModalExample/ModalExample.js
@@ -2,50 +2,54 @@ import React, {Component} from 'react';
 import {AppRegistry, Modal, Text, TouchableHighlight, View, Alert} from 'react-native';
 
 export default class ModalExample extends Component {
-  constructor(){
-    super();
-    this.state = {
-        modalVisible: false
+
+    constructor(){
+        super();
+        this.state = {
+            modalVisible: false
+        }
     }
-  }
 
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
-  }
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+    }
 
-  render() {
-    return (
-      <View style={{marginTop: 22}}>
-        <Modal
-          animationType="fade"
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-          }}>
-          <View style={{marginTop: 22}}>
-            <View>
-              <Text>Hello World!</Text>
+    render() {
+        return (
+            <View style={{marginTop: 22}}>
+                <Modal
+                    animationType="fade"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                    }}
+                >
+                    <View style={{marginTop: 22}}>
+                        <View>
+                            <Text>Hello World!</Text>
 
-              <TouchableHighlight
-                onPress={() => {
-                  this.setModalVisible(!this.state.modalVisible);
-                }}>
-                <Text>Hide Modal</Text>
-              </TouchableHighlight>
+                            <TouchableHighlight
+                                onPress={() => {
+                                    this.setModalVisible(!this.state.modalVisible);
+                                }}
+                            >
+                                <Text>Hide Modal</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </Modal>
+
+                <TouchableHighlight
+                    onPress={() => {
+                        this.setModalVisible(true);
+                    }}
+                >
+                    <Text>Show Modal</Text>
+                </TouchableHighlight>
             </View>
-          </View>
-        </Modal>
-
-        <TouchableHighlight
-          onPress={() => {
-            this.setModalVisible(true);
-          }}>
-          <Text>Show Modal</Text>
-        </TouchableHighlight>
-      </View>
-    );
-  }
+        );
+    }
 }
 
 AppRegistry.registerComponent('ModalExample, () => ModalExample');
