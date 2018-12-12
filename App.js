8f8043e7bd8f51238f8f207cade14011b873c2e9
Ramdhani <dhaniverdant23@gmail.com>
2018-12-12 19:08:59 +0700

    try to load ProgressBarAndroid

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import {AppRegistry, Text, View} from 'react-native';
 
-import ModalExample from './src/components/ModalExample/ModalExample';
+import PickerExample from './src/components/PickerExample/PickerExample';
 
 export default class MyApp extends Component{
 
   render(){
     return(
-      <ModalExample/>
+      <PickerExample/>
     );
   }
 }
