1ea225168c95eca96089003de7955b896bce80b5
Ramdhani <dhaniverdant23@gmail.com>
2018-12-18 11:52:02 +0700

    AndroidPager

--- src/components/NativeFeedbackComponent/NativeFeedbackComponent.js
+++ src/components/NativeFeedbackComponent/NativeFeedbackComponent.js
@@ -13,7 +13,7 @@ export default class NativeFeedbackComponent extends Component {
               onPress={this._onPressButton}
               background={TouchableNativeFeedback.SelectableBackground()}>
             <View style={{width: 150, height: 100, backgroundColor: 'red'}}>
-              <Text style={{margin: 30}}>Button</Text>
+              <Text style={{margin: 30}}>Press</Text>
             </View>
           </TouchableNativeFeedback>
         );
